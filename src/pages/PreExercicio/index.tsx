9a5b5f9f1ae081a427f062b4318606a778a68e61
Christyelen <christyelenk@gmail.com>
2023-04-22 20:19:03 -0300

    remoção de código que da erro

--- src/pages/PreExercicio/index.tsx
+++ src/pages/PreExercicio/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View , Text, TouchableOpacity} from "react-native"
+import { View, Text, TouchableOpacity } from "react-native"
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { propsStack } from "../../routes/Stack/Models";
 import { styles } from "./styles";
@@ -11,43 +11,16 @@ const PreExercicio = () => {
     let video2 = "";
     let video3 = "";
 
-    const validarNivelExercicio = () => {
-    //    if(params.params.nivel == '1'){
-    //     console.log("entrou");
-    //     video1 = "testeeee";
-    //     video2 = "";
-    //     video3 = "";
-    //    }
-    //    else if(params.params.nivel == '2'){
-    //     video1 = "";
-    //     video2 = "";
-    //     video3 = "";
-    //    } 
-    //    else if(params.params.nivel == '3'){
-    //     video1 = "";
-    //     video2 = "";
-    //     video3 = "";
-    //    } 
-    //    else if(params.params.nivel == '4'){
-    //     video1 = "";
-    //     video2 = "";
-    //     video3 = "";
-    //    }
-
-       console.log()
-       navigation.navigate("Exercicio", { idVideo1: video1, idVideo2: video2, idVideo03: video3})
-    }
-    
-    return(
+    return (
         <View style={styles.container}>
-            <Text style={{fontSize:20}}>Pre Exercicio</Text>
+            <Text style={{ fontSize: 20 }}>Pre Exercicio</Text>
 
-            <TouchableOpacity 
-            style={styles.buttom}
-            onPress={() =>{ validarNivelExercicio()}}>
+            <TouchableOpacity
+                style={styles.buttom}
+                onPress={() => {navigation.navigate("Exercicio", { idVideo1: video1, idVideo2: video2, idVideo03: video3 }) }}>
                 <Text>Exercicio</Text>
             </TouchableOpacity>
-         
+
             <TouchableOpacity
                 style={styles.buttom}
                 onPress={() => navigation.goBack()}>
