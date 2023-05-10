99665cb7911f60eb56e5b14e64faaa84df8a900b
Christyelen <christyelenk@gmail.com>
2023-05-10 00:34:19 -0300

    finalização de criação de coleções no firebase

--- src/pages/FichaAnamnese/index.tsx
+++ src/pages/FichaAnamnese/index.tsx
@@ -4,10 +4,11 @@ import { useNavigation } from "@react-navigation/native"
 import { propsStack } from "../../routes/Stack/Models";
 import { TextInput, Button, Checkbox } from 'react-native-paper';
 import { styles } from "./styles";
-import { addDoc, collection, onSnapshot} from "firebase/firestore";
+import { addDoc, collection, doc, getFirestore, onSnapshot, updateDoc } from "firebase/firestore";
 import { FIRESTORE_DB } from "../../../firebaseConfig";
 
 const FichaAnamnese = (props) => {
+
     let teste = props.route.params.name;
     const navigation = useNavigation<propsStack>()
     const [isVisibleSaude, setIsVisibleSaude] = useState(false);
@@ -44,27 +45,52 @@ const FichaAnamnese = (props) => {
 
     const [fichaAnamnese, setFichaAnamnese] = useState('');
 
-    // const atualizarCampos = async () => {
-    //     try {
-    //         const materiasRef = collection(FIRESTORE_DB, 'fichaAnamnese');
-    //         const subscriber = onSnapshot(materiasRef, {
-    //             next: (snapshot) => {
-    //                 const materias: any[] = [];
-    //                 snapshot.docs.filter((doc) => '' == doc.id)
-    //                 .forEach((doc) => {
-    //                     materias.push({
-    //                         id: doc.id,
-    //                         ...doc.data()
-    //                     })
-    //                 });
-    //                 setFichaAnamnese(materias)
-    //             }
-    //         });
-    //         return () => subscriber();
-    //     } catch (error) {
-    //         alert(error.message);
-    //     }
-    // };
+    const atualizarCampos = async () => {
+        
+        //codigo comentado da erro era a tentativa de buscar o id do documento com query 
+
+        //const db = firestore()
+        //console.log(db)
+        //const colecaoRef = db.collection('fichaAnamnese')
+        // Executar a consulta para encontrar o documento com base em algum critério de pesquisa
+        //const querySnapshot = await colecaoRef.where('nome', '==', 'chris').get();
+        //console.log(querySnapshot.docs[0].ref);
+        // Atualizar o primeiro documento retornado pela consulta
+        //if (!querySnapshot.empty) {
+        //  const documentoRef = querySnapshot.docs[0].ref;
+        const docRef = await doc(FIRESTORE_DB, 'fichaAnamnese', '7zCVJzege5OE65Ecknk8')
+
+        updateDoc(docRef, {
+            nome: nome,
+            sobrenome: sobrenome,
+            cpf: cpf,
+            rg: rg,
+            dataNascimento: dataNascimento,
+            sexo: sexo,
+            peso: peso,
+            altura: altura,
+            telefone: telefone,
+            email: email,
+            anoDiagnostico: anoDiagnostico,
+            possuiPlano: possuiPlano,
+            cartaoPlano: cartaoPlano,
+            usoRemedios: usoRemedios,
+            remediosUtilizados: remediosUtilizados,
+            cep: cep,
+            estado: estado,
+            cidade: cidade,
+            bairro: bairro,
+            rua: rua,
+            numero: numero,
+            medicoResponsavel: medicoResponsavel,
+            crmMedico: crmMedico,
+            possuiCuidador: possuiCuidador,
+            nomeCuidador: nomeCuidador,
+            cpfCuidador: cpfCuidador
+        })
+        setFichaAnamnese('');
+        console.log('Passou update')
+    };
 
     const addFicha = async () => {
         const doc = await addDoc(collection(FIRESTORE_DB, 'fichaAnamnese'), {
@@ -94,13 +120,10 @@ const FichaAnamnese = (props) => {
             possuiCuidador: possuiCuidador,
             nomeCuidador: nomeCuidador,
             cpfCuidador: cpfCuidador
-
-
         });
-        console.log('Passou')
         setFichaAnamnese('');
+        console.log('Passou salvar')
     }
-
 
     const mostrarCampoSaude = () => {
         setIsVisibleSaude(!isVisibleSaude);
@@ -123,7 +146,12 @@ const FichaAnamnese = (props) => {
 
     const salvarCampos = () => {
         if (!isReadOnly) {
-            addFicha();
+            if (true) { //fazer validação se ja possui um ID no banco
+                atualizarCampos();
+            } else {
+                addFicha();
+            }
+
         }
         setarCamposReadOnly();
     }
--- src/pages/Ofensiva/index.tsx
+++ src/pages/Ofensiva/index.tsx
@@ -1,15 +1,49 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { View, Text, TouchableOpacity, SafeAreaView, ScrollView, Share } from "react-native"
 import { useNavigation } from "@react-navigation/native";
 import { propsStack } from "../../routes/Stack/Models";
 import { styles } from "./styles";
 import { Appbar, Button } from "react-native-paper";
 import ViewShot from 'react-native-view-shot';
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import { FIRESTORE_DB } from "../../../firebaseConfig";
 
 const Ofensiva = () => {
     const navigation = useNavigation<propsStack>()
-
     const viewShotRef = useRef(null);
+    const [listaOfensivas, setlistaOfensivas] = useState([]);
+    const [diasOfensiva, setDiasOfensiva] = useState('');
+
+    useEffect(() => {
+        buscarOfensivas();
+        calcularDiasDeOfensiva();
+    }, []);
+
+    const buscarOfensivas = async () => {
+        try {
+            const ofensivasRef = collection(FIRESTORE_DB, 'ofensiva');
+            const subscriber = onSnapshot(ofensivasRef, {
+                next: (snapshot) => {
+                    const ofensivas: any[] = [];
+                    snapshot.docs.forEach((doc) => {
+                        ofensivas.push({
+                            id: doc.id,
+                            ...doc.data()
+                        })
+                    });
+                    setlistaOfensivas(ofensivas)
+                }
+            });
+            return () => subscriber();
+        } catch (error) {
+            alert(error.message);
+        }
+    };
+
+    const calcularDiasDeOfensiva = () =>{
+        setDiasOfensiva('2');
+        //fazer controle da quantidade de dias de ofensiva
+    }
 
     const handleShare = async () => {
 
@@ -50,8 +84,10 @@ const Ofensiva = () => {
                     </View>
                     <ViewShot ref={viewShotRef} style={{ flex: 1 }}>
                         <View style={styles.container}>
-                            <Text style={styles.textGroup}>Você está com uma ofensiva de: 6 dias</Text>
-                            <Text style={{ margin: 10 }}>Parabéns bla bla bla</Text>
+                            <Text style={styles.textGroup}>Você está com uma ofensiva de: {diasOfensiva} dias</Text>
+                            {listaOfensivas.map(ofensiva => (
+                                <Text style={{ margin: 10 }}>ofensiva: {ofensiva.dataExercicioRealizado}</Text>
+                            ))}
                             <Text style={{ margin: 10 }}>~Colocar calendáriozinho com os dias que ja foram marcados e um icone de foguinho~</Text>
                         </View>
                     </ViewShot>
